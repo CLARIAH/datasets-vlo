c2029bfa5e87b1dc87c0cf73a1aba84e55ef7ea6
Twan Goosen <twan@clarin.eu>
2018-02-12 16:06:46 +0100

    Added compose configuration for a content service the metadata records as static file content

--- docker-compose-beta.sh
+++ docker-compose-beta.sh
@@ -2,4 +2,4 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash "${DIR}/check-hostname.sh" "beta-vlo-clarin.esc.rzg.mpg.de"
-bash "${DIR}/docker-compose.sh" -f "beta.yml" -f "jmx.yml" $@
+bash "${DIR}/docker-compose.sh" -f "beta.yml" -f "jmx.yml" -f "data.yml" $@
--- docker-compose-testing.sh
+++ docker-compose-testing.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-bash "${DIR}/docker-compose.sh" -f "testing.yml" -f "jmx.yml" $@
+bash "${DIR}/docker-compose.sh" -f "testing.yml" -f "jmx.yml" -f "data.yml" $@
