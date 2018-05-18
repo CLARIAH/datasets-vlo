91a6c22c87eadb0201e1ab6d81cf6f471d8c4921
Willem Elbers <wjm.elbers@gmail.com>
2018-05-18 13:44:10 +0200

    Reverted again to run compose command in subshell

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,8 +4,8 @@ DIR=$(dirname $BASH_SOURCE)
 if [ $(readlink $0) ]; then
         DIR=$(dirname $(readlink $0))
 fi
-cd "${DIR}/clarin" \
+$(cd "${DIR}/clarin" \
   && docker-compose \
     -f docker-compose.yml \
-    $@
+    $@)
 
