bdf1a821b55117f3f8e860a89ef0e8b4e696170f
Twan Goosen <twan@clarin.eu>
2022-01-11 09:29:18 +0100

    added nginx to compose tests

--- test/run-test.sh
+++ test/run-test.sh
@@ -2,19 +2,22 @@
 set -e
 
 POSTFIX_NETWORKNAME=postfix_mail
+COMPOSE_OPTS=(--env-file .env -f ../clarin/docker-compose.yml -f ../clarin/nginx.yml -f compose-test-override.yml)
 
-docker-compose --env-file .env -f ../clarin/docker-compose.yml -f compose-test-override.yml down -v
+docker-compose "${COMPOSE_OPTS[@]}" down -v
 if [ $(docker network ls |grep "${POSTFIX_NETWORKNAME}"|wc -l)  -eq 0 ]; then
     docker network create "${POSTFIX_NETWORKNAME}"
 fi
 
 echo "Starting application..."
-docker-compose --env-file .env -f ../clarin/docker-compose.yml -f compose-test-override.yml up
+docker-compose "${COMPOSE_OPTS[@]}" up
 
 #Verify all containers are closed nicely
-number_of_failed_containers="$(docker-compose --env-file .env -f ../clarin/docker-compose.yml -f compose-test-override.yml \
-    ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | grep -c 0 -v | tr -d ' ')"
+number_of_failed_containers="$(docker-compose "${COMPOSE_OPTS[@]}" ps -q \
+    | xargs docker inspect -f '{{ .State.ExitCode }}' \
+    | grep -c 0 -v \
+    | tr -d ' ')"
 #cleanup
-docker-compose --env-file .env -f ../clarin/docker-compose.yml -f compose-test-override.yml down -v
+docker-compose "${COMPOSE_OPTS[@]}" down -v
 
 exit "$number_of_failed_containers"
